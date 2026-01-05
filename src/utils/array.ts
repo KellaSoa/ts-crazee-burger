81fbba8b250e0f7e2db6c04f41ca3cfb13b4f289
kella <fk.rianja@gmail.com>
2026-01-05 10:09:23 +0100

    TSE-23: ORDER > CARD > add types > utils

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,33 +1,30 @@
-export const deepClone = <T>(array: T[]): T[] => {
-  return JSON.parse(JSON.stringify(array));
-};
+import { Category } from "@/types/Category"
 
-export const findObjectById = <T extends { id: string }>(
-  id: string,
-  array: T[]
-): T | undefined => {
-  return array.find((itemInArray) => itemInArray.id === id);
-};
+export const deepClone = <T>(array: T[]): T[] => {
+  return JSON.parse(JSON.stringify(array))
+}
+
+export const findObjectById = <T extends { id: string }>(id: string, array: T[]): T | undefined => {
+  return array.find((itemInArray) => itemInArray.id === id)
+}
 
 export const findIndexById = <T extends { id: string }>(
-  idWithUnknowwIndex: string,
+  idWithUnknownIndex: string,
   array: T[]
 ): number => {
-  return array.findIndex(
-    (itemInArray) => itemInArray.id === idWithUnknowwIndex
-  );
-};
+  return array.findIndex((itemInArray) => itemInArray.id === idWithUnknownIndex)
+}
 
 export const removeObjectById = <T extends { id: string }>(
   idOfItemToRemove: string,
   array: T[]
 ): T[] => {
-  return array.filter((item) => item.id !== idOfItemToRemove);
-};
+  return array.filter((item) => item.id !== idOfItemToRemove)
+}
 
 export const isEmpty = <T>(array: T[]): boolean => {
-  return array.length === 0;
-};
+  return array.length === 0
+}
 
 // const fruits = [{ nom: "Abricot" }, { nom: "Banane" }]
 // const fruitsShallowCopy = [...fruits]
@@ -39,3 +36,7 @@ export const isEmpty = <T>(array: T[]): boolean => {
 // console.log("fruits: ", fruits)
 // //console.log("fruitsShallowCopy: ", fruitsShallowCopy)
 // console.log("fruitsDeepCopy: ", fruitsDeepCopy)
+
+export const getCategoryActive = (categories: Category[]): Category | undefined => {
+  return categories.find((category) => category.isActive)
+}
